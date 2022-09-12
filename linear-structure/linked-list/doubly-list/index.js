2c8dbca07fd4b130add3be4d21f1615654e164d5
Alex-Li2018 <392064881@qq.com>
2022-09-12 22:37:50 +0800

    feat: 双向链表反转

--- linear-structure/linked-list/doubly-list/index.js
+++ linear-structure/linked-list/doubly-list/index.js
@@ -70,6 +70,23 @@ class DoublyList {
 
     this.length -= 1
   }
+
+  reverse(curr) {
+    if (curr.next === null) {
+      this.head = new LinkNode(null)
+      this.head.next = curr
+      this.tail = null
+      curr.prev = null
+      return curr
+    }
+
+    const pre = this.reverse(curr.next)
+    pre.next = curr
+    curr.next = null
+    curr.prev = pre
+
+    return pre.next
+  }
 }
 
 const link = new DoublyList()
@@ -77,6 +94,7 @@ link.add(1)
 link.add(2)
 link.add(4)
 link.insert(2,3)
-link.remove(1)
+// link.remove(1)
+link.reverse(link.head.next)
 console.log(link)
 
